afaa60661bfca706f5ceade684ae9c6fa92521b4
Luis Abraham <luis.abraham22@hotmail.com>
2025-10-14 20:32:26 -0400

    Revert "Lexer and Cursor implementation"
    This reverts commit 26684e56f46b74f698b477f9b5f3d6ed7d3fcbe8.

--- cmd/repl/main.go
+++ cmd/repl/main.go
@@ -1,24 +1,8 @@
 package main
 
-import (
-	"fmt"
-	"luisabraham22/lovelang/core/lexer"
-	"luisabraham22/lovelang/core/token"
-)
+import "fmt"
 
 func main() {
 
-	l := lexer.New("let x = fn(str, foo) {}")
-
-	for {
-		tok := l.NextToken()
-
-		fmt.Printf("%s\n", tok)
-
-		if tok.Type == token.EOF {
-			break
-
-		}
-	}
-
+	fmt.Println("Hello, world!")
 }
--- core/token/token.go
+++ core/token/token.go
@@ -8,95 +8,37 @@ type Token struct {
 }
 
 const (
-	Illegal    TokenType = "ILLEGAL"
-	EOF                  = "EOF"
-	Identifier           = "IDENTIFIER"
-
-	Integer = "INT"
-	String  = "STRING"
+	EOF = "EOF"
 
 	// Punctuation
-	Comma      TokenType = "COMMA"
-	Semicolon  TokenType = "SEMICOLON"
-	LeftParen  TokenType = "LEFT_PAREN"
-	RightParen TokenType = "RIGHT_PAREN"
-	LeftBrace  TokenType = "LEFT_BRACE"
-	RightBrace TokenType = "RIGHT_BRACE"
-
-	// Operators
-	Assign              TokenType = "ASSIGN"
-	Plus                TokenType = "PLUS"
-	Equals              TokenType = "EQUALS"
-	NotEquals           TokenType = "NOT_EQUALS"
-	Not                 TokenType = "NOT"
-	Asterisk            TokenType = "ASTERISK"
-	Minus               TokenType = "MINUS"
-	Slash               TokenType = "SLASH"
-	LessThan            TokenType = "LESS_THAN"
-	LessThanOrEquals    TokenType = "LESS_THAN_OR_EQUALS"
-	GreaterThan         TokenType = "GREATER_THAN"
-	GreaterThanOrEquals TokenType = "GREATER_THAN_OR_EQUALS"
-
-	// Keywords
-	Function TokenType = "FUNCTION"
-	Let      TokenType = "LET"
-	True     TokenType = "TRUE"
-	False    TokenType = "FALSE"
-	If       TokenType = "IF"
-	Else     TokenType = "ELSE"
-	Return   TokenType = "RETURN"
-	For      TokenType = "FOR"
+	Comma      TokenType = ","
+	Semicolon  TokenType = ";"
+	LeftParen  TokenType = "("
+	RightParen TokenType = ")"
+	LeftBrace  TokenType = "{"
+	RightBrace TokenType = "}"
 )
 
-var keywords = map[string]TokenType{
-	"fn":     Function,
-	"let":    Let,
-	"true":   True,
-	"false":  False,
-	"if":     If,
-	"else":   Else,
-	"return": Return,
-	"for":    For,
-}
+func FromLexeme(ch string) Token {
+	var tokenType TokenType = EOF
 
-var operators = map[string]TokenType{
-	"=":  Assign,
-	"+":  Plus,
-	"==": Equals,
-	"!=": NotEquals,
-	"!":  Not,
-	"*":  Asterisk,
-	"-":  Minus,
-	"/":  Slash,
-	"<":  LessThan,
-	"<=": LessThanOrEquals,
-	">":  GreaterThan,
-	">=": GreaterThanOrEquals,
-}
-
-var punctuation = map[string]TokenType{
-	",": Comma,
-	";": Semicolon,
-	"(": LeftParen,
-	")": RightParen,
-	"{": LeftBrace,
-	"}": RightBrace,
-}
-
-func LookupIdentifier(literal string) TokenType {
-	if token, ok := keywords[literal]; ok {
-		return token
+	switch ch {
+	case ",":
+		tokenType = Comma
+	case ";":
+		tokenType = Semicolon
+	case "(":
+		tokenType = LeftParen
+	case ")":
+		tokenType = RightParen
+	case "{":
+		tokenType = LeftBrace
+	case "}":
+		tokenType = RightBrace
 	}
 
-	return Identifier
-}
-
-func LookupOperator(literal string) (TokenType, bool) {
-	t, ok := operators[literal]
-	return t, ok
-}
-
-func LookupPunctuation(literal string) (TokenType, bool) {
-	t, ok := punctuation[literal]
-	return t, ok
+	return Token{
+		tokenType,
+		ch,
+	}
 }
